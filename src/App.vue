f31d98049a72c05c3abc6be5c513b6efef56a8d4
techchrism <chris@techchrism.me>
2022-05-10 10:39:11 -0700

    begin adding sidebar

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,22 @@
             app
             color="primary"
             dark
+            clipped-left
         >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
             <div class="d-flex align-center">
-                <h2>Mobile Robotics Lab</h2>
+                <h2>Mobile Robotics Lab Telemetry</h2>
             </div>
 
             <v-spacer></v-spacer>
 
             <WebsocketConnection/>
         </v-app-bar>
+
+        <v-navigation-drawer app v-model="drawer" absolute clipped>
+            <FrameSidebar/>
+        </v-navigation-drawer>
 
         <v-main>
             <HelloWorld/>
@@ -23,17 +30,19 @@
 <script>
 import HelloWorld from './components/HelloWorld';
 import WebsocketConnection from "@/components/WebsocketConnection";
+import FrameSidebar from "@/components/FrameSidebar";
 
 export default {
     name: 'App',
 
     components: {
+        FrameSidebar,
         WebsocketConnection,
         HelloWorld,
     },
 
     data: () => ({
-        //
+        drawer: null
     }),
 };
 </script>
---
+++ src/components/FrameSidebar.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+            <v-list-item>
+                <v-list-item-title>Foo</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-title>Bar</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-title>Fizz</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item>
+                <v-list-item-title>Buzz</v-list-item-title>
+            </v-list-item>
+        </v-list-item-group>
+    </v-list>
+</template>
+
+<script>
+export default {
+    name: 'FrameSidebar',
+    data: () => ({
+        group: null
+    })
+}
+</script>
+
+<style scoped>
+
+</style>
